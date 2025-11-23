c944d44b4ed0736044a4842f0ede90b9f1fc984b
ValeryA26 <andy21211309@gmail.com>
2025-11-23 12:25:53 -0500

    pantalla funcional
    se han puesto los botones ya en la pantalla, y se han conectado las dos escenas (primera escena funcional únicamente) (no se visualiza la escena aún)

---
+++ Escenas/controlMenuDeOpciones.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_p_financiero_pressed() -> void:
+	get_tree().change_scene_to_file("res://Escenas/transaccion.tscn")
+
+func _on_alm_datos_pressed() -> void:
+	print("Almacenamiento de datos como trabajo futuro")
+
+func _on_ay_compras_pressed() -> void:
+	print("Ayuda en compras virtuales como trabajo futuro")
+
+func _on_busqweb_pressed() -> void:
+	print("Búsqueda en web como trabajo futuro")
